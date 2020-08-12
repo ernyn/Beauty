141fe5c85548dc1e85dd394536ef901e884904f2
yano <yano.erina@m.alhinc.jp>
2020-08-12 11:07:17 +0900

    first commit

--- src/main/java/com/example/demo/WeightForm.java
+++ src/main/java/com/example/demo/WeightForm.java
@@ -1,12 +1,12 @@
 package com.example.demo;
 
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.Pattern;
+//import javax.validation.constraints.NotEmpty;
+//import javax.validation.constraints.Pattern;
 
 public class WeightForm {
 
-	@NotEmpty(message = " 身長を入力して下さい")
-	@Pattern(regexp = "\\d{3}", message = "半角数字、3ケタでお願いいたします")
+//	@NotEmpty(message = " 身長を入力して下さい")
+//	@Pattern(regexp = "\\d+(\\.\\d+)?", message = "半角数字、3ケタでお願いいたします")
 	private double number;
 
 	public double getNumber() {
